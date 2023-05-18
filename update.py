7b34fecaeb960a3fe82ac6b50889b5e3f3b25f08
MrUser-404 <131967259+MrUser-404@users.noreply.github.com>
2023-05-18 14:53:53 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,8 @@
 import os,time
 print("\033[1;97m[\033[1;91m•\033[1;97m]Wait for update\033[0m")
-os.system('clear')
 time.sleep(2)
+os.system('clear')
+
 os.system('clear')
 print("\033[1;97m[\033[1;92m•\033[1;97m]Wait for update\033[0m")
 time.sleep(2)
