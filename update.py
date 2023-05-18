24e7edd9350acc0e9fbeed08fb342db1dd5ba9e3
MrUser-404 <131967259+MrUser-404@users.noreply.github.com>
2023-05-18 14:46:08 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 import os,time
 print("\033[1;97m[\033[1;91m•\033[1;97m]Wait for update\033[0m")
+os.system('clear')
 time.sleep(2)
 os.system('clear')
 print("\033[1;97m[\033[1;92m•\033[1;97m]Wait for update\033[0m")
@@ -9,5 +10,6 @@ os.system('cd && rm -rf Token && git clone https://github.com/MrUser-404/Token')
 os.system('clear')
 print('\033[1;97m[\033[1;92m✓\033[1;97m]Success For Update\033[0m')
 time.sleep(2)
-os.system('clear')
-os.system('python exec.py')
+exit()
+cd Token 
+python exec.py
