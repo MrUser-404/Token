fbfe67719430a259ec0f126dc9b5fd2dda2ecb38
MrUser-404 <131967259+MrUser-404@users.noreply.github.com>
2023-05-18 14:15:55 +0300

    Add files via upload

---
+++ exec.py
@@ -0,0 +1,14 @@
+import platform,time,os
+x=platform.architecture()[0]
+if x=="64bit":
+	os.system('clear')
+	print("\033[1;97m[\033[0m\033[1;91m✓\033[0m\033[1;97m]\033[7;92m64bit Found\033[0m")
+	time.sleep(2)
+	import Token64
+	Token64.tutorial()
+elif x=="32bit":
+	os.system('clear')
+	print("\033[1;97m[\033[1;91m✓\033[1;97m]\033[0m\033[7;92m32bit Found\033[0m")
+	time.sleep(2)
+	import Token32
+	Token32.tutorial()
---
+++ update tool.py
@@ -0,0 +1,12 @@
+import os,time
+print("\033[1;97m[\033[1;91m•\033[1;97m]Wait for update\033[0m")
+time.sleep(2)
+os.system('clear')
+print("\033[1;97m[\033[1;92m•\033[1;97m]Wait for update\033[0m")
+time.sleep(2)
+os.system('clear')
+os.system('cd && rm -rf Token && git clone https://github.com/MrUser-404/Token')
+os.system('clear')
+print('\033[1;97m[\033[1;92m✓\033[1;97m]Success For Update\033[0m')
+os.system('clear')
+os.system('cd Token && python exec.py')
